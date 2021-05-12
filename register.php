b7e23fc4d0e920c2b166acf448343d0b4f418b62
Alejandro Pestana <alejandropestana@outlook.com>
2021-05-12 18:51:03 -0400

    Creación inicial de página de registro

---
+++ register.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bienvenido a Carlevix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+    </head>
+    <body>
+        prueba
+    </body>
+
+</html>
