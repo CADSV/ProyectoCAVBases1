763203e2ea61a49938f104750fe15453554723b1
Alejandro Pestana <alejandropestana@outlook.com>
2021-05-12 19:47:21 -0400

    Agregados inputs y su estilo

--- register.php
+++ register.php
@@ -1,3 +1,9 @@
+<?php
+    if(isset($_POST["botonEnviar"])) { // Si el botón Enviar es presionado, entonces...
+        echo "El formulario ha sido enviado con éxito";
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,7 +11,31 @@
         <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
     </head>
     <body>
-        prueba
+        <div class="signInContainer">
+            <div class="column">
+                <form method="POST"> <!-- El método post sirve para enviar datos -->
+                    <input type="text" name="nombre" placeholder="Nombre(s)" required> <!-- required hace que sea necesario llenar el campo antes de enviar -->
+
+                    <input type="text" name="apellido" placeholder="Apellido(s)" required>
+
+                    <input type="text" name="nombreUsuario" placeholder="Nombre de usuario" required>
+
+                    <input type="email" name="correo" placeholder="Correo electrónico" required>
+
+                    <input type="email" name="correo2" placeholder="Confirmar correo electrónico" required>
+
+                    <input type="password" name="contraseña" placeholder="Contraseña" required>
+
+                    <input type="password" name="contraseña2" placeholder="Confirmar contraseña" required>
+
+                    <input type="submit" name="botonEnviar" value="ENVIAR">
+
+                </form>
+
+            </div>
+
+        </div>
+
     </body>
 
 </html>
