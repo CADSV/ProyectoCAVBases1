4c9a3c38cede33d545515c97666875113a381866
Alejandro Pestana <alejandropestana@outlook.com>
2021-05-13 00:28:40 -0400

    Finalizada la página de registro y de login, acorde con la sección 2. También agregados algunos detalles, como a los botones y links

--- login.php
+++ login.php
@@ -16,30 +16,21 @@
 
                 <div class="header">
                     <img src="assets/images/logo.png" title="Logo" alt="Logo de la página"> <!-- title provee una especie de tooltip, que al poner el cursor encima de la imagen revela un texto, esto ayuda a la accesibilidad, al igual que alt cuando la imagen no cargue -->
-                    <h3>Regístrese</h3>
+                    <h3>Inicia sesión</h3>
                     <span>para continuar a Carlevix</span>
                 </div>
 
-                <form method="POST"> <!-- El método post sirve para enviar datos -->
-                    <input type="text" name="nombre" placeholder="Nombre(s)" required> <!-- required hace que sea necesario llenar el campo antes de enviar -->
-
-                    <input type="text" name="apellido" placeholder="Apellido(s)" required>
+                <form method="POST">
 
                     <input type="text" name="nombreUsuario" placeholder="Nombre de usuario" required>
 
-                    <input type="email" name="correo" placeholder="Correo electrónico" required>
-
-                    <input type="email" name="correo2" placeholder="Confirmar correo electrónico" required>
-
                     <input type="password" name="contraseña" placeholder="Contraseña" required>
 
-                    <input type="password" name="contraseña2" placeholder="Confirmar contraseña" required>
-
-                    <input type="submit" class="button" name="botonEnviar" value="ENVIAR">
+                    <input type="submit" class="button" name="botonIniciarSesion" value="Iniciar sesión">
 
                 </form>
 
-                <a href="login.php" class="signInMessage">¿Ya estás registrado? Inicia sesión aquí</a>
+                <a href="register.php" class="signUpMessage">¿No estás registrado? Regístrate aquí</a>
 
             </div>
 
