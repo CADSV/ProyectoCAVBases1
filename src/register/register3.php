e14538da2cdbb875d09f66e7a8a362e59d98f4fd
Carlos Doffiny S-V <47164433+CADSV@users.noreply.github.com>
2021-05-30 11:51:05 -0400

    Creando el archivo de registro 3 y su archivo de estilos

---
+++ src/register/register3.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" 
+        content="width=device-width, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0, user-scalable=no">
+    <title>Registro 3</title>
+    <link rel="stylesheet" type="text/css" href="../../assets/style/registerStyle.css">
+</head>
+<body>
+    
+</body>
+</html>
+
