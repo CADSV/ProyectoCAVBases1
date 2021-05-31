c72baa64b194999b442a85d32bebda8f80fe25a9
LAPTOP-4J8K1UJL\Vicente <vicentemirabal16@gmail.com>
2021-05-30 20:48:00 -0400

    Register 1 listo

---
+++ src/register/register1.php
@@ -0,0 +1,55 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" 
+        content="width=device-width, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0, user-scalable=no">
+    <title>Registro 1</title>
+    <link rel="stylesheet" type="text/css" href="../../assets/style/registerStyle.css">
+</head>
+<body>
+    <div class="wrapper">
+
+        <header>
+            <div class="login-header">
+                <a href="../../index.php"> 
+                    <img src="../../assets/images/logo.png" title="Logo" alt="Logo de la página"> 
+                </a>          
+            </div>
+            <nav class = "login">
+                <a href="../login/login.php">Inicia Sesión</a>
+            </nav>
+        </header>
+        <div class = "line"></div>
+    </div>
+        <section>
+            <div class = "container">
+                <div class = "checkbox">
+                    <img src="../../assets/images/check.png" title="check" alt="check">
+
+                </div>
+
+                <div class = "title">
+                    <p>PASO 1 DE 3</p>
+                </div>
+                <div class = "subtitle">
+                    <p>Selecciona tu plan</p>
+                </div>
+                <div class = "text">
+                </p>  <img src="../../assets/images/check_sin_circulo.png" title="check_sin_circulo" alt="check_sin_circulo" align="left">   Sin compromisos, cancela cuando quieras.</p>
+                </p>  <img src="../../assets/images/check_sin_circulo.png" title="check_sin_circulo" alt="check_sin_circulo" align="left">   Todo Carlevix a un bajo costo.</p>
+                </p>   <img src="../../assets/images/check_sin_circulo.png" title="check_sin_circulo" alt="check_sin_circulo" align="left">   Disfruta sin límites en todos tus dispositivos.</p>
+                </div>
+                <div class = "buttonContainer">
+                    <a href="register.php">Ver los planes</a>
+                </div>
+            </div>
+        </section>
+    
+</body>
+</html>
