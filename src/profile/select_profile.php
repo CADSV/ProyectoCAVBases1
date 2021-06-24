7c635f44b1bee049ec73d88f7096e1d2fe4f69c4
Carlos Doffiny S-V <47164433+CADSV@users.noreply.github.com>
2021-06-24 17:19:45 -0400

    Creada la interfaz de seleccionar perfil con su respectivo estilo

---
+++ src/profile/select_profile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0, user-scalable=no">
+    <title>Selecciona tu perfil</title>
+    <link rel="stylesheet" href="../../assets/style/profileStyle.css">
+</head>
+
+<body>
+    <header>
+        <a href="../../index.php"> 
+            <img src="../../assets/images/logo.png" title="Logo" alt="Logo de la página"> 
+        </a>          
+    </header>
+
+    <section>
+        <div class = "container">
+
+        </div>
+
+    </section>
+
+
+</body>
+</html>
