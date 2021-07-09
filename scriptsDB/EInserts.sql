2db43573af2bbdc7aa473294ce548fc47ff63f02
Carlos Doffiny S-V <47164433+CADSV@users.noreply.github.com>
2021-07-08 21:43:34 -0400

    bd

--- scriptsDB/EInserts.sql
+++ scriptsDB/EInserts.sql
@@ -12,13 +12,13 @@ INSERT INTO paymentcard VALUES (1878126903615428,163,'Hassan','Nasrallah','2025-
 
 --IsSuscribed--
 INSERT INTO issuscribed VALUES (1,3,123,6276559076321199,'2021-07-08 14:01:01',NULL);
-UPDATE user SET UserIsSuscribed = 1 WHERE IdUser = 1;
+UPDATE user SET UserIsSuscribed = 1, UserPostalCode = 1111, UserAvenueStreet = 'Paraiso', UserBuildingHouse = 'Las Palmas' WHERE IdUser = 1;
 
 INSERT INTO issuscribed VALUES (2,3,000,1122334455667788,'2021-07-08 14:01:01',NULL);
-UPDATE user SET UserIsSuscribed = 1 WHERE IdUser = 2;
+UPDATE user SET UserIsSuscribed = 1, UserPostalCode = 1298, UserAvenueStreet = 'Carora Avenue', UserBuildingHouse = 'El Casino' WHERE IdUser = 2;
 
 INSERT INTO issuscribed VALUES (3,3,858,0102030405060708,'2021-07-08 14:01:01',NULL); 
-UPDATE user SET UserIsSuscribed = 1 WHERE IdUser = 3;
+UPDATE user SET UserIsSuscribed = 1, UserPostalCode = 1080, UserAvenueStreet = 'Manzanares Oeste', UserBuildingHouse = 'Casa Grande' WHERE IdUser = 3;
 
 INSERT INTO issuscribed VALUES (6,1,112,9900886673651286,'2021-10-18 23:13:56',NULL); 
 UPDATE user SET UserIsSuscribed = 1 WHERE IdUser = 6;
@@ -30,4 +30,4 @@ INSERT INTO issuscribed VALUES (10,2,666,0001000200030004,'2022-01-03 09:53:27',
 UPDATE user SET UserIsSuscribed = 1 WHERE IdUser = 10;
 
 INSERT INTO issuscribed VALUES (4,3,163,1878126903615428,'2021-07-11 22:22:01',NULL);
-UPDATE user SET UserIsSuscribed = 1 WHERE IdUser = 4;-- INSERCIÓN E: Solo 7 usuarios han hecho formal su inscripción.
+UPDATE user SET UserIsSuscribed = 1 WHERE IdUser = 4;
