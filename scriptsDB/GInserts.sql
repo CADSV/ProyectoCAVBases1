902e0d7a18049ca768dd03aa764248e8868964e3
LAPTOP-4J8K1UJL\Vicente <vicentemirabal16@gmail.com>
2021-07-08 22:36:51 -0400

    Creacion de episodios de The Crown

--- scriptsDB/GInserts.sql
+++ scriptsDB/GInserts.sql
@@ -1,1 +1,49 @@
 -- INSERCIÓN G: Cada temporada puede estar compuesta por un mínimo de 7 capítulos 
+--Episodios
+
+
+--Episodios The Crown Temporada 1
+INSERT INTO episode VALUES (1,1,27,'Wolferton Splash','0:40:00');
+INSERT INTO episode VALUES (2,1,27,'Hyde Park Corner','0:40:00');
+INSERT INTO episode VALUES (3,1,27,'Windsor','0:40:00');
+INSERT INTO episode VALUES (4,1,27,'Fuerza mayor','0:40:00');
+INSERT INTO episode VALUES (5,1,27,'Humo y espejos','0:33:00');
+INSERT INTO episode VALUES (6,1,27,'Noticia bomba','0:30:00');
+INSERT INTO episode VALUES (7,1,27,'Scientia potentia est','0:30:00');
+
+--Episodios The Crown Temporada 2
+INSERT INTO episode VALUES (1,2,27,'Orgullo y alegría','0:35:00');
+INSERT INTO episode VALUES (2,2,27,'Asesinos','0:35:00');
+INSERT INTO episode VALUES (3,2,27,'Gloriana','0:35:00');
+INSERT INTO episode VALUES (4,2,27,'Desventura','0:35:00');
+INSERT INTO episode VALUES (5,2,27,'Compañía de hombres','0:35:00');
+INSERT INTO episode VALUES (6,2,27,'Marionetas','0:35:00');
+INSERT INTO episode VALUES (7,2,27,'Vergangenheit','0:30:00');
+
+--Episodios The Crown Temporada 3
+INSERT INTO episode VALUES (1,3,27,'Matrimonium','0:25:00');
+INSERT INTO episode VALUES (2,3,27,'Estimada Sra. Kennedy','0:25:00');
+INSERT INTO episode VALUES (3,3,27,'Paterfamilias','0:28:00');
+INSERT INTO episode VALUES (4,3,27,'Hombre misterioso','0:28:00');
+INSERT INTO episode VALUES (5,3,27,'Olding','0:25:00');
+INSERT INTO episode VALUES (6,3,27,'Marganismo','0:29:00');
+INSERT INTO episode VALUES (7,3,27,'Aberfan','0:20:00');
+
+--Episodios The Crown Temporada 4
+
+INSERT INTO episode VALUES (1,4,27,'Bubbikins','0:20:00');
+INSERT INTO episode VALUES (2,4,27,'Golpe de estado','0:25:00');
+INSERT INTO episode VALUES (3,4,27,'Tywysog Cymru','0:28:00');
+INSERT INTO episode VALUES (4,4,27,'Polvo lunar','0:28:00');
+INSERT INTO episode VALUES (5,4,27,'El hombre en suspenso','0:29:00');
+INSERT INTO episode VALUES (6,4,27,'Enredo','0:25:00');
+INSERT INTO episode VALUES (7,4,27,'Grito de amor','0:25:00');
+
+--Episodios The Crown Temporada 5
+INSERT INTO episode VALUES (1,5,27,'Protector de monarcas','0:30:00');
+INSERT INTO episode VALUES (2,5,27,'El Balmoral','0:35:00');
+INSERT INTO episode VALUES (3,5,27,'El favorito','0:38:00');
+INSERT INTO episode VALUES (4,5,27,'Michael Fagan, el intruso','0:38:00');
+INSERT INTO episode VALUES (5,5,27,'Tierra de nadie','0:39:00');
+INSERT INTO episode VALUES (6,5,27,'Lo que se hereda','0:35:00');
+INSERT INTO episode VALUES (7,5,27,'48 contra 1','0:25:00');
