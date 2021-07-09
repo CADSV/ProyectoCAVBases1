785bb6422058bd97b8cf03f30b5afa3fca0dbdf4
Alejandro Pestana <alejandropestana@outlook.com>
2021-07-09 00:03:39 -0400

    Episodios de Vicente

--- scriptsDB/insert/GInserts.sql
+++ scriptsDB/insert/GInserts.sql
@@ -1,1 +1,191 @@
 -- INSERCIÓN G: Cada temporada puede estar compuesta por un mínimo de 7 capítulos 
+--Episodios
+
+
+--Episodios The Crown Temporada 1
+INSERT INTO episode VALUES (1,1,27,'Wolferton Splash','0:40:00');
+INSERT INTO episode VALUES (2,1,27,'Hyde Park Corner','0:40:00');
+INSERT INTO episode VALUES (3,1,27,'Windsor','0:40:00');
+INSERT INTO episode VALUES (4,1,27,'Fuerza mayor','0:40:00');
+INSERT INTO episode VALUES (5,1,27,'Humo y espejos','0:33:00');
+INSERT INTO episode VALUES (6,1,27,'Noticia bomba','0:30:00');
+INSERT INTO episode VALUES (7,1,27,'Scientia potentia est','0:30:00');
+
+--Episodios The Crown Temporada 2
+INSERT INTO episode VALUES (1,2,27,'Orgullo y alegría','0:35:00');
+INSERT INTO episode VALUES (2,2,27,'Asesinos','0:35:00');
+INSERT INTO episode VALUES (3,2,27,'Gloriana','0:35:00');
+INSERT INTO episode VALUES (4,2,27,'Desventura','0:35:00');
+INSERT INTO episode VALUES (5,2,27,'Compañía de hombres','0:35:00');
+INSERT INTO episode VALUES (6,2,27,'Marionetas','0:35:00');
+INSERT INTO episode VALUES (7,2,27,'Vergangenheit','0:30:00');
+
+--Episodios The Crown Temporada 3
+INSERT INTO episode VALUES (1,3,27,'Matrimonium','0:25:00');
+INSERT INTO episode VALUES (2,3,27,'Estimada Sra. Kennedy','0:25:00');
+INSERT INTO episode VALUES (3,3,27,'Paterfamilias','0:28:00');
+INSERT INTO episode VALUES (4,3,27,'Hombre misterioso','0:28:00');
+INSERT INTO episode VALUES (5,3,27,'Olding','0:25:00');
+INSERT INTO episode VALUES (6,3,27,'Marganismo','0:29:00');
+INSERT INTO episode VALUES (7,3,27,'Aberfan','0:20:00');
+
+--Episodios The Crown Temporada 4
+
+INSERT INTO episode VALUES (1,4,27,'Bubbikins','0:20:00');
+INSERT INTO episode VALUES (2,4,27,'Golpe de estado','0:25:00');
+INSERT INTO episode VALUES (3,4,27,'Tywysog Cymru','0:28:00');
+INSERT INTO episode VALUES (4,4,27,'Polvo lunar','0:28:00');
+INSERT INTO episode VALUES (5,4,27,'El hombre en suspenso','0:29:00');
+INSERT INTO episode VALUES (6,4,27,'Enredo','0:25:00');
+INSERT INTO episode VALUES (7,4,27,'Grito de amor','0:25:00');
+
+--Episodios The Crown Temporada 5
+INSERT INTO episode VALUES (1,5,27,'Protector de monarcas','0:30:00');
+INSERT INTO episode VALUES (2,5,27,'El Balmoral','0:35:00');
+INSERT INTO episode VALUES (3,5,27,'El favorito','0:38:00');
+INSERT INTO episode VALUES (4,5,27,'Michael Fagan, el intruso','0:38:00');
+INSERT INTO episode VALUES (5,5,27,'Tierra de nadie','0:39:00');
+INSERT INTO episode VALUES (6,5,27,'Lo que se hereda','0:35:00');
+INSERT INTO episode VALUES (7,5,27,'48 contra 1','0:25:00');
+
+
+
+
+
+
+
+
+
+
+--Episodios The last Kingdom Temporada 1
+INSERT INTO episode VALUES (1,1,29,'El secuestro','0:40:00');
+INSERT INTO episode VALUES (2,1,29,'El tío Aelfic','0:40:00');
+INSERT INTO episode VALUES (3,1,29,'Reencuentro con Breda','0:40:00');
+INSERT INTO episode VALUES (4,1,29,'La reina pagana','0:30:00');
+INSERT INTO episode VALUES (5,1,29,'Enemigo imponente','0:40:00');
+INSERT INTO episode VALUES (6,1,29,'La batalla decisiva','1:00:00');
+INSERT INTO episode VALUES (7,1,29,'Victorioso al ataque','0:50:00');
+
+--Episodios last Kingdom Temporada 2
+INSERT INTO episode VALUES (1,2,29,'Enviada al norte','0:55:00');
+INSERT INTO episode VALUES (2,2,29,'Las estratagemas','0:55:00');
+INSERT INTO episode VALUES (3,2,29,'El reino de mercia','0:55:00');
+INSERT INTO episode VALUES (4,2,29,'Northhumbria','0:55:00');
+INSERT INTO episode VALUES (5,2,29,'Beocca','0:50:00');
+INSERT INTO episode VALUES (6,2,29,'A la guerra','0:45:00');
+INSERT INTO episode VALUES (7,2,29,'La humillación','0:45:00');
+
+--Episodios last Kingdom Temporada 3
+INSERT INTO episode VALUES (1,3,29,'El reto','0:25:00');
+INSERT INTO episode VALUES (2,3,29,'Desconfianza','0:35:00');
+INSERT INTO episode VALUES (3,3,29,'El error','0:38:00');
+INSERT INTO episode VALUES (4,3,29,'El retorno','0:38:00');
+INSERT INTO episode VALUES (5,3,29,'La huida inesperada','0:35:00');
+INSERT INTO episode VALUES (6,3,29,'Animales salvajes','0:39:00');
+INSERT INTO episode VALUES (7,3,29,'Victoria sin pelea','0:30:00');
+
+--Episodios last Kingdom Temporada 4
+
+INSERT INTO episode VALUES (1,4,29,'La debilidad','0:20:00');
+INSERT INTO episode VALUES (2,4,29,'La mira en el trono','0:25:00');
+INSERT INTO episode VALUES (3,4,29,'La neblina en la ruta','0:28:00');
+INSERT INTO episode VALUES (4,4,29,'El dilema','0:28:00');
+INSERT INTO episode VALUES (5,4,29,'Plan de batalla','0:29:00');
+INSERT INTO episode VALUES (6,4,29,'Division interna','0:25:00');
+INSERT INTO episode VALUES (7,4,29,'¿Arriesgarse valdrá la pena?','0:25:00');
+
+--Episodios last Kingdom Temporada 5
+INSERT INTO episode VALUES (1,5,29,'Ataque escocés','0:20:00');
+INSERT INTO episode VALUES (2,5,29,'La aproximación','0:15:00');
+INSERT INTO episode VALUES (3,5,29,'Se unen las tropas','0:18:00');
+INSERT INTO episode VALUES (4,5,29,'Ataques a Bebbaburg','0:18:00');
+INSERT INTO episode VALUES (5,5,29,'El arrepentimiento','0:29:00');
+INSERT INTO episode VALUES (6,5,29,'La conquista llega','0:19:00');
+INSERT INTO episode VALUES (7,5,29,'Celebracion de tropas','0:19:00');
+
+
+
+
+
+--Episodios de series con una temporada
+
+--Episodios de Lupin
+
+INSERT INTO episode VALUES (1,1,21,'Robo improvisado','0:19:00');
+INSERT INTO episode VALUES (2,1,21,'El escape','0:19:00');
+INSERT INTO episode VALUES (3,1,21,'La entrevista','0:19:00');
+INSERT INTO episode VALUES (4,1,21,'La entrega','0:19:00');
+INSERT INTO episode VALUES (5,1,21,'Tiempos de analisis','0:19:00');
+INSERT INTO episode VALUES (6,1,21,'Viaje con acompañante','0:19:00');
+INSERT INTO episode VALUES (7,1,21,'Ataque en lo mas profundo','0:17:00');
+
+
+--Episodios de La niña
+
+INSERT INTO episode VALUES (1,1,22,'La captura','0:22:00');
+INSERT INTO episode VALUES (2,1,22,'El cine','0:22:00');
+INSERT INTO episode VALUES (3,1,22,'Emboscada','0:22:00');
+INSERT INTO episode VALUES (4,1,22,'Viaje extraño','0:22:00');
+INSERT INTO episode VALUES (5,1,22,'Lugar desconocido','0:22:00');
+INSERT INTO episode VALUES (6,1,22,'Sabiduría','0:22:00');
+INSERT INTO episode VALUES (7,1,22,'La visita','0:23:00');
+
+
+--Episodios de La Bella y las Bestias
+INSERT INTO episode VALUES (1,1,23,'La odisea de Isabela','01:27:00');
+INSERT INTO episode VALUES (2,1,23,'El autor intelectual','01:27:00');
+INSERT INTO episode VALUES (3,1,23,'Las pruebas del asesino','01:27:00');
+INSERT INTO episode VALUES (4,1,23,'Mike y Penelope','01:27:00');
+INSERT INTO episode VALUES (5,1,23,'El video de las bestias','01:27:00');
+INSERT INTO episode VALUES (6,1,23,'El camino a la justicia','01:27:00');
+INSERT INTO episode VALUES (7,1,23,'Terrenos abiertos','01:27:00');
+
+
+--Episodios de Sobreviviendo a Escobar
+INSERT INTO episode VALUES (1,1,24,'El Popeye en la jugada','0:23:00');
+INSERT INTO episode VALUES (2,1,24,'La llegada','0:23:00');
+INSERT INTO episode VALUES (3,1,24,'La boda a todo dar','0:23:00');
+INSERT INTO episode VALUES (4,1,24,'El regalo del patrón','0:23:00');
+INSERT INTO episode VALUES (5,1,24,'Empieza el negocio','0:23:00');
+INSERT INTO episode VALUES (6,1,24,'El Popeye escritor','0:23:00');
+INSERT INTO episode VALUES (7,1,24,'La positiva oyó','0:29:00');
+
+
+--Episodios de La Ley Secreta
+INSERT INTO episode VALUES (1,1,25,'La operación','1:45:00');
+INSERT INTO episode VALUES (2,1,25,'Agentes encubiertos','1:45:00');
+INSERT INTO episode VALUES (3,1,25,'La frustración','1:45:00');
+INSERT INTO episode VALUES (4,1,25,'El equipo descubierto','1:45:00');
+INSERT INTO episode VALUES (5,1,25,'La improvisación','1:45:00');
+INSERT INTO episode VALUES (6,1,25,'La oferta','1:45:00');
+INSERT INTO episode VALUES (7,1,25,'El fin de la operación','1:48:00');
+
+
+--Episodios de Merlin
+INSERT INTO episode VALUES (1,1,26,'The dragon´s call','0:46:00');
+INSERT INTO episode VALUES (2,1,26,'Valiant','0:46:00');
+INSERT INTO episode VALUES (3,1,26,'The Mark of Nimueh','0:46:00');
+INSERT INTO episode VALUES (4,1,26,'The poisoned Chalice','0:46:00');
+INSERT INTO episode VALUES (5,1,26,'Lancelot','0:46:00');
+INSERT INTO episode VALUES (6,1,26,'Remedy to Cure All ills','0:46:00');
+INSERT INTO episode VALUES (7,1,26,'The Gates of Avalon','0:48:00');
+
+
+--Episodios de Carta al Rey
+INSERT INTO episode VALUES (1,1,28,'Se avecina una tormenta','0:34:00');
+INSERT INTO episode VALUES (2,1,28,'¿No es un encanto?','0:34:00');
+INSERT INTO episode VALUES (3,1,28,'En el fin del mundo','0:34:00');
+INSERT INTO episode VALUES (4,1,28,'Caballeros peligrosos','0:34:00');
+INSERT INTO episode VALUES (5,1,28,'Espiral','0:32:00');
+INSERT INTO episode VALUES (6,1,28,'Luna de sangre','0:32:00');
+INSERT INTO episode VALUES (7,1,28,'La embarcación','0:32:00');
+
+
+--Episodios de No hables con extraños
+INSERT INTO episode VALUES (1,1,30,'Un desconocido se acerca','2:45:00');
+INSERT INTO episode VALUES (2,1,30,'Ataque juvenil','2:45:00');
+INSERT INTO episode VALUES (3,1,30,'La confesión','2:45:00');
+INSERT INTO episode VALUES (4,1,30,'El descubrimiento','2:45:00');
+INSERT INTO episode VALUES (5,1,30,'Un violento enfrentamiento','2:45:00');
+INSERT INTO episode VALUES (6,1,30,'Un insolito aliado','2:45:00');
+INSERT INTO episode VALUES (7,1,30,'Reunión inseperada','2:45:00');
