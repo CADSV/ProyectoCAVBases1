19a969e578e750f8496f7870f106d551698259bd
Carlos Doffiny S-V <47164433+CADSV@users.noreply.github.com>
2021-07-08 21:28:21 -0400

    bd scripts

--- scriptsDB/CInsertsParte1.sql
+++ scriptsDB/CInsertsParte1.sql
@@ -1,25 +1,24 @@
--- INSERCIÓN C: 20 Películas, de las cuales 5 son contenido original. 
-INSERT INTO ageclass VALUES (0,'Todo público');
-INSERT INTO language VALUES (1,'Inglés');
+-- INSERCIÓN C: 20 Películas, de las cuales 5 son contenido original. -------------------------------------------------------------------------------
+INSERT INTO ageclass VALUES (0,'Todo público');sINSERT INTO language VALUES (1,'Inglés');
 
 INSERT INTO content VALUES (1, 0, 1);
-INSERT INTO featurecontent VALUES (1,'Duro de Matar',1988,0,1,'Una persona cuya vida es difícil de arrebatar salva al mundo','2:11:00',); -- Acción
+INSERT INTO featurecontent VALUES (1,'Duro de Matar',1988,DEFAULT,1,'Una persona cuya vida es difícil de arrebatar salva al mundo','2:11:00',); -- Acción
 INSERT INTO content VALUES (2, 0, 1);
-INSERT INTO featurecontent VALUES (2,'Nomadland',2020,0,1,'Una nómada deja su hogar y viaja por los Estados Unidos','1:48:00',); -- Drama, premio Oscar mejor película
+INSERT INTO featurecontent VALUES (2,'Nomadland',2020,DEFAULT,1,'Una nómada deja su hogar y viaja por los Estados Unidos','1:48:00',); -- Drama, premio Oscar mejor película
 INSERT INTO content VALUES (3, 0, 1);
-INSERT INTO featurecontent VALUES (3,'Blade Runner',1982,0,1,'En el futuro, la bioingeniería permite la fabricación de humanos artificiales','1:57:00',); -- Neo-noir y Ciencia ficción
+INSERT INTO featurecontent VALUES (3,'Blade Runner',1982,DEFAULT,1,'En el futuro, la bioingeniería permite la fabricación de humanos artificiales','1:57:00',); -- Neo-noir y Ciencia ficción
 INSERT INTO content VALUES (4, 0, 1);
-INSERT INTO featurecontent VALUES (4,'Ava',2020,0,1,'Ava, alguna vez una militar y adicta, es una asesina a sueldo','1:37:00',); -- Acción y Suspenso
+INSERT INTO featurecontent VALUES (4,'Ava',2020,DEFAULT,1,'Ava, alguna vez una militar y adicta, es una asesina a sueldo','1:37:00',); -- Acción y Suspenso
 INSERT INTO content VALUES (5, 0, 1);
 INSERT INTO featurecontent VALUES (5,'Las Desventuras de Rodrigo: La Película',2008,1,1,'Rodrigo debe luchar para sobrevivir ante los malévolos villanos que se avecinan','2:32:01',); -- Acción y Suspenso
 INSERT INTO content VALUES (6, 0, 1);
 INSERT INTO featurecontent VALUES (6,'Un Refugio Inesperado',2017,1,1,'Una familia protege a los judíos en un zoológico durante la Segunda Guerra Mundial','2:07:00',); -- Actua Daniel Bruhl, Drama y Guerra
 INSERT INTO content VALUES (7, 0, 1);
-INSERT INTO featurecontent VALUES (7,'Los Infiltrados',2006,0,1,'En Boston, la mafia y la policía chocan','2:31:00',); -- Oscar mejor película, Crimen y Suspenso
+INSERT INTO featurecontent VALUES (7,'Los Infiltrados',2006,DEFAULT,1,'En Boston, la mafia y la policía chocan','2:31:00',); -- Oscar mejor película, Crimen y Suspenso
 INSERT INTO content VALUES (8, 0, 1);
-INSERT INTO featurecontent VALUES (8,'Vuela Alto',2005,0,1,'Un superhéroe debe decidir entre salvar al mundo y salvar a su hija','2:44:00',); -- Superhéroes
+INSERT INTO featurecontent VALUES (8,'Vuela Alto',2005,DEFAULT,1,'Un superhéroe debe decidir entre salvar al mundo y salvar a su hija','2:44:00',); -- Superhéroes
 INSERT INTO content VALUES (9, 0, 1);
-INSERT INTO featurecontent VALUES (9,'¡A Correr!',2021,0,1,'Ante una catástrofe, la gente debe huir','1:56:00',); -- Suspenso
+INSERT INTO featurecontent VALUES (9,'¡A Correr!',2021,DEFAULT,DEFAULT,'Ante una catástrofe, la gente debe huir','1:56:00',); -- Suspenso
 INSERT INTO content VALUES (10, 0, 1);
 INSERT INTO featurecontent VALUES (10,'¡Ayuda, Ahí Vienen!',2016,1,1,'Un día los zombies aparecen y destruyen a la humanidad','2:45:00',); -- Premio golden globe mejor actor, Acción y Terror
 INSERT INTO content VALUES (11, 0, 1);
@@ -33,12 +32,12 @@ INSERT INTO featurecontent VALUES (14,'Tu Casa es Su Casa',2013,0,1,'Un criminal
 INSERT INTO content VALUES (15, 0, 1);
 INSERT INTO featurecontent VALUES (15,'Mesa Verde',2001,0,1,'Un banco quiebra en medio de una crisis financiera','1:55:00',); --Drama
 INSERT INTO content VALUES (16, 0, 1);
-INSERT INTO featurecontent VALUES (16,'La Base de Datos',2021,0,1,'Un estudiante de ingeniería informática debe hacer una base de datos para guardar la información del mundo entero','2:11:00',); --Ciencia ficción
+INSERT INTO featurecontent VALUES (16,'La Base de Datos',2021,DEFAULT,1,'Un estudiante de ingeniería informática debe hacer una base de datos para guardar la información del mundo entero','2:11:00',); --Ciencia ficción
 INSERT INTO content VALUES (17, 0, 1);
-INSERT INTO featurecontent VALUES (17,'El Señor de los Anillos: el retorno del Rey',2003,0,1,'','3:21:00',); -- Premio óscar mejor película, Aventura y Fantasía
+INSERT INTO featurecontent VALUES (17,'El Señor de los Anillos: el retorno del Rey',2003,DEFAULT,1,'','3:21:00',); -- Premio óscar mejor película, Aventura y Fantasía
 INSERT INTO content VALUES (18, 0, 1);
 INSERT INTO featurecontent VALUES (18,'Viaje al Sol',2005,1,1,'Un grupo de astronautas debe viajar al Sol para salvar al sistema solar','2:44:00',); -- Ciencia ficción
 INSERT INTO content VALUES (19, 0, 1);
 INSERT INTO featurecontent VALUES (19,'El Domingo No',2020,1,1,'Un señor quiere pasar sus domingos tranquilo, pero sus vecinos no lo dejan','1:26:00',); -- Comedia
 INSERT INTO content VALUES (20, 0, 1);
-INSERT INTO featurecontent VALUES (20,'Capitán América: Civil War',2016,0,1,'Los Vengadores se pelean entre sí','2:27:00',); -- Actua Daniel Bruhl, Superhéroes
+INSERT INTO featurecontent VALUES (20,'Capitán América: Civil War',2016,DEFAULT,1,'Los Vengadores se pelean entre sí','2:27:00',); -- Actua Daniel Bruhl, Superhéroes
