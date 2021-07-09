d13bdc174da9f249a0aef6e266cd0dc5a37bc0e8
LAPTOP-4J8K1UJL\Vicente <vicentemirabal16@gmail.com>
2021-07-08 23:09:06 -0400

    Update GInserts.sql
    Creacion de capitulos de The last Kingdom

--- scriptsDB/GInserts.sql
+++ scriptsDB/GInserts.sql
@@ -47,3 +47,58 @@ INSERT INTO episode VALUES (4,5,27,'Michael Fagan, el intruso','0:38:00');
 INSERT INTO episode VALUES (5,5,27,'Tierra de nadie','0:39:00');
 INSERT INTO episode VALUES (6,5,27,'Lo que se hereda','0:35:00');
 INSERT INTO episode VALUES (7,5,27,'48 contra 1','0:25:00');
+
+
+
+
+
+
+
+
+
+
+--Episodios The last Kingdom Temporada 1
+INSERT INTO episode VALUES (1,1,29,'El secuestro','0:40:00');
+INSERT INTO episode VALUES (2,1,29,'El tío Aelfic','0:40:00');
+INSERT INTO episode VALUES (3,1,29,'Reencuentro con Breda','0:40:00');
+INSERT INTO episode VALUES (4,1,29,'La reina pagana','0:30:00');
+INSERT INTO episode VALUES (5,1,29,'Enemigo imponente','0:40:00');
+INSERT INTO episode VALUES (6,1,29,'La batalla decisiva','1:00:00');
+INSERT INTO episode VALUES (7,1,29,'Victorioso al ataque','0:50:00');
+
+--Episodios last Kingdom Temporada 2
+INSERT INTO episode VALUES (1,2,29,'Enviada al norte','0:55:00');
+INSERT INTO episode VALUES (2,2,29,'Las estratagemas','0:55:00');
+INSERT INTO episode VALUES (3,2,29,'El reino de mercia','0:55:00');
+INSERT INTO episode VALUES (4,2,29,'Northhumbria','0:55:00');
+INSERT INTO episode VALUES (5,2,29,'Beocca','0:50:00');
+INSERT INTO episode VALUES (6,2,29,'A la guerra','0:45:00');
+INSERT INTO episode VALUES (7,2,29,'La humillación','0:45:00');
+
+--Episodios last Kingdom Temporada 3
+INSERT INTO episode VALUES (1,3,29,'El reto','0:25:00');
+INSERT INTO episode VALUES (2,3,29,'Desconfianza','0:35:00');
+INSERT INTO episode VALUES (3,3,29,'El error','0:38:00');
+INSERT INTO episode VALUES (4,3,29,'El retorno','0:38:00');
+INSERT INTO episode VALUES (5,3,29,'La huida inesperada','0:35:00');
+INSERT INTO episode VALUES (6,3,29,'Animales salvajes','0:39:00');
+INSERT INTO episode VALUES (7,3,29,'Victoria sin pelea','0:30:00');
+
+--Episodios last Kingdom Temporada 4
+
+INSERT INTO episode VALUES (1,4,29,'La debilidad','0:20:00');
+INSERT INTO episode VALUES (2,4,29,'La mira en el trono','0:25:00');
+INSERT INTO episode VALUES (3,4,29,'La neblina en la ruta','0:28:00');
+INSERT INTO episode VALUES (4,4,29,'El dilema','0:28:00');
+INSERT INTO episode VALUES (5,4,29,'Plan de batalla','0:29:00');
+INSERT INTO episode VALUES (6,4,29,'Division interna','0:25:00');
+INSERT INTO episode VALUES (7,4,29,'¿Arriesgarse valdrá la pena?','0:25:00');
+
+--Episodios last Kingdom Temporada 5
+INSERT INTO episode VALUES (1,5,29,'Ataque escocés','0:20:00');
+INSERT INTO episode VALUES (2,5,29,'La aproximación','0:15:00');
+INSERT INTO episode VALUES (3,5,29,'Se unen las tropas','0:18:00');
+INSERT INTO episode VALUES (4,5,29,'Ataques a Bebbaburg','0:18:00');
+INSERT INTO episode VALUES (5,5,29,'El arrepentimiento','0:29:00');
+INSERT INTO episode VALUES (6,5,29,'La conquista llega','0:19:00');
+INSERT INTO episode VALUES (7,5,29,'Celebracion de tropas','0:19:00');
