392b6a8aff4b118745d4159a0f2bc0019918d000
Alejandro Pestana <alejandropestana@outlook.com>
2021-07-10 14:04:25 -0400

    Corridos los números de country y city por 1

--- scriptsDB/insert/BInserts.sql
+++ scriptsDB/insert/BInserts.sql
@@ -1,65 +1,65 @@
 -- INSERCIÓN B: 10 Países, y 3 ciudades en cada uno.-------------------------------------------------------------------------
-INSERT INTO country VALUES (1,"Venezuela");
-INSERT INTO country VALUES (2,"Bélgica");
-INSERT INTO country VALUES (3,"Francia");
-INSERT INTO country VALUES (4,"Italia");
-INSERT INTO country VALUES (5,"Alemania");
-INSERT INTO country VALUES (6,"Colombia");
-INSERT INTO country VALUES (7,"Portugal");
-INSERT INTO country VALUES (8,"Libano");
-INSERT INTO country VALUES (9,"Cuba");
-INSERT INTO country VALUES (10,"Corea del Norte");
+INSERT INTO country VALUES (2,"Venezuela");
+INSERT INTO country VALUES (3,"Bélgica");
+INSERT INTO country VALUES (4,"Francia");
+INSERT INTO country VALUES (5,"Italia");
+INSERT INTO country VALUES (6,"Alemania");
+INSERT INTO country VALUES (7,"Colombia");
+INSERT INTO country VALUES (8,"Portugal");
+INSERT INTO country VALUES (9,"Libano");
+INSERT INTO country VALUES (10,"Cuba");
+INSERT INTO country VALUES (11,"Corea del Norte");
 
 -- VENEZUELA
-INSERT INTO city VALUES (1,1,'Caracas');
-INSERT INTO city VALUES (2,1,'Barquisimeto');
-INSERT INTO city VALUES (3,1,'Valencia');
+INSERT INTO city VALUES (1,2,'Caracas');
+INSERT INTO city VALUES (2,2,'Barquisimeto');
+INSERT INTO city VALUES (3,2,'Valencia');
 
 -- BÉLGICA
-INSERT INTO city VALUES (4,2,'Bruselas');
-INSERT INTO city VALUES (5,2,'Charleroi');
-INSERT INTO city VALUES (6,2,'Vlamerting');
+INSERT INTO city VALUES (4,3,'Bruselas');
+INSERT INTO city VALUES (5,3,'Charleroi');
+INSERT INTO city VALUES (6,3,'Vlamerting');
 
 -- FRANCIA
-INSERT INTO city VALUES (7,3,'París');
-INSERT INTO city VALUES (8,3,'Montpellier');
-INSERT INTO city VALUES (9,3,'Lyon');
+INSERT INTO city VALUES (7,4,'París');
+INSERT INTO city VALUES (8,4,'Montpellier');
+INSERT INTO city VALUES (9,4,'Lyon');
 
 -- ITALIA
-INSERT INTO city VALUES (10,4,'Roma');
-INSERT INTO city VALUES (11,4,'Milán');
-INSERT INTO city VALUES (12,4,'Venecia');
+INSERT INTO city VALUES (10,5,'Roma');
+INSERT INTO city VALUES (11,5,'Milán');
+INSERT INTO city VALUES (12,5,'Venecia');
 
 
 -- ALEMANIA
-INSERT INTO city VALUES (13,5,'Berlín');
-INSERT INTO city VALUES (14,5,'Münich');
-INSERT INTO city VALUES (15,5,'Köln');
+INSERT INTO city VALUES (13,6,'Berlín');
+INSERT INTO city VALUES (14,6,'Münich');
+INSERT INTO city VALUES (15,6,'Köln');
 
 -- COLOMBIA
-INSERT INTO city VALUES (16,6,'Bogotá');
-INSERT INTO city VALUES (17,6,'Cali');
-INSERT INTO city VALUES (18,6,'Medellín');
+INSERT INTO city VALUES (16,7,'Bogotá');
+INSERT INTO city VALUES (17,7,'Cali');
+INSERT INTO city VALUES (18,7,'Medellín');
 
 
 -- PORTUGAL
-INSERT INTO city VALUES (19,7,'Lisboa');
-INSERT INTO city VALUES (20,7,'Funchal');
-INSERT INTO city VALUES (21,7,'Porto');
+INSERT INTO city VALUES (19,8,'Lisboa');
+INSERT INTO city VALUES (20,8,'Funchal');
+INSERT INTO city VALUES (21,8,'Porto');
 
 -- LÍBANO
-INSERT INTO city VALUES (22,8,'Beirut');
-INSERT INTO city VALUES (23,8,'Trípoli');
-INSERT INTO city VALUES (24,8,'Sidón');
+INSERT INTO city VALUES (22,9,'Beirut');
+INSERT INTO city VALUES (23,9,'Trípoli');
+INSERT INTO city VALUES (24,9,'Sidón');
 
 
 -- CUBA
-INSERT INTO city VALUES (25,9,'La Habana');
-INSERT INTO city VALUES (26,9,'Varadero');
-INSERT INTO city VALUES (27,9,'Cienfuegos');
+INSERT INTO city VALUES (25,10,'La Habana');
+INSERT INTO city VALUES (26,10,'Varadero');
+INSERT INTO city VALUES (27,10,'Cienfuegos');
 
 
 -- COREA DEL NORTE
-INSERT INTO city VALUES (28,10,'Piongyang');
-INSERT INTO city VALUES (29,10,'Kaesong');
-INSERT INTO city VALUES (30,10,'Sinuiju');
+INSERT INTO city VALUES (28,11,'Piongyang');
+INSERT INTO city VALUES (29,11,'Kaesong');
+INSERT INTO city VALUES (30,11,'Sinuiju');
