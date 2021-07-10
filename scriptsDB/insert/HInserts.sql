3b4e33c21833ec0e6d8c9eedabc1cd3dd840bba0
Carlos Doffiny S-V <47164433+CADSV@users.noreply.github.com>
2021-07-10 13:58:47 -0400

    Cambiados los IDs para que los ID 1 sean los Default

--- scriptsDB/insert/HInserts.sql
+++ scriptsDB/insert/HInserts.sql
@@ -1,21 +1,20 @@
 -- INSERCIÓN H: Al menos 10 actores.--------------------------------------------------------------------------
 
 -- FilmWorker--
-INSERT INTO filmworker VALUES (1,'Rodolfo','Rodríguez','M');
-INSERT INTO filmworker VALUES (2,'Daniel','Craig','M');
-INSERT INTO filmworker VALUES (3,'Emma','Stone','F');
-INSERT INTO filmworker VALUES (4,'Leonardo','DiCaprio','M');
-INSERT INTO filmworker VALUES (5,'Rachel','McAdams','F');
-INSERT INTO filmworker VALUES (6,'Jennifer','Aniston','F');
-INSERT INTO filmworker VALUES (7,'Scarlett','Johansson','F');
-INSERT INTO filmworker VALUES (8,'Chris','Evans','M');
-INSERT INTO filmworker VALUES (9,'Keanu','Reeves','M');
-INSERT INTO filmworker VALUES (10,'Jordana','Brewster','F');
-INSERT INTO filmworker VALUES (11,'Daniel','Brühl','M');
+INSERT INTO filmworker VALUES (2,'Rodolfo','Rodríguez','M');
+INSERT INTO filmworker VALUES (3,'Daniel','Craig','M');
+INSERT INTO filmworker VALUES (4,'Emma','Stone','F');
+INSERT INTO filmworker VALUES (5,'Leonardo','DiCaprio','M');
+INSERT INTO filmworker VALUES (6,'Rachel','McAdams','F');
+INSERT INTO filmworker VALUES (7,'Jennifer','Aniston','F');
+INSERT INTO filmworker VALUES (8,'Scarlett','Johansson','F');
+INSERT INTO filmworker VALUES (9,'Chris','Evans','M');
+INSERT INTO filmworker VALUES (10,'Keanu','Reeves','M');
+INSERT INTO filmworker VALUES (11,'Jordana','Brewster','F');
+INSERT INTO filmworker VALUES (12,'Daniel','Brühl','M');
 
 
 -- Performer--
-INSERT INTO performer VALUES (1);
 INSERT INTO performer VALUES (2);
 INSERT INTO performer VALUES (3);
 INSERT INTO performer VALUES (4);
@@ -26,3 +25,4 @@ INSERT INTO performer VALUES (8);
 INSERT INTO performer VALUES (9);
 INSERT INTO performer VALUES (10);
 INSERT INTO performer VALUES (11);
+INSERT INTO performer VALUES (12);
