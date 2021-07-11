281d9df5b9b0c849d87e9221158909425d4a174a
Alejandro Pestana <alejandropestana@outlook.com>
2021-07-11 14:24:54 -0400

    Style de errorMessage

--- data/account/registerAccount.php
+++ data/account/registerAccount.php
@@ -91,7 +91,7 @@ class RegisterAccount{
 
     public function getError($error){
         if (in_array($error, $this->errorArray)){
-            return $error;
+            return "<span class='errorMessage'>$error</span>";
         }
     }
 
