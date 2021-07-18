fe249b23814d32a9b2dbe0c3b16ce5417dbf4723
LAPTOP-4J8K1UJL\Vicente <vicentemirabal16@gmail.com>
2021-07-18 09:16:22 -0400

    Se agregó la barra de Navegación

---
+++ src/content/movies.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once("../../data/config.php");
+
+/*if (!isset($_SESSION["userLoggedIn"])){
+    header("Location: ../register/register1.php");
+}
+
+$userLoggedIn = $_SESSION["userLoggedIn"];*/
+
+?>
+    <!DOCTYPE html>
+    <html lang='es'>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport"
+                content="width=device-width, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0, user-scalable=no">
+            <title>Carlevix</title>
+            <link rel="stylesheet" type="text/css" href="../../assets/style/navBarStyle.css"/> 
+            <script src="https://kit.fontawesome.com/06a651c8da.js" crossorigin="anonymous"> </script>
+
+        </head>
+        <body>
+            <div class="wrapper">
+
+               <?php
+               if(!isset($hideNav)){
+                   include_once("navBar.php");
+               }
+               ?>
+            </div>
+        </div>   
+    </html>         
+
+       
