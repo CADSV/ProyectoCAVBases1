128e5eb457c055c6eb9667fd5921d6d309a90181
LAPTOP-4J8K1UJL\Vicente <vicentemirabal16@gmail.com>
2021-07-24 09:35:45 -0400

    Se agregaron filmworkers y directores

--- scriptsDB/insert/HInserts.sql
+++ scriptsDB/insert/HInserts.sql
@@ -12,26 +12,42 @@ INSERT INTO filmworker VALUES (8,'Chris','Evans','M');
 INSERT INTO filmworker VALUES (9,'Keanu','Reeves','M');
 INSERT INTO filmworker VALUES (10,'Jordana','Brewster','F');
 INSERT INTO filmworker VALUES (11,'Daniel','Brühl','M');
+
+
+
 -- Agregados 
 INSERT INTO filmworker VALUES (12,'Clint','Eastwood','M');
 INSERT INTO filmworker VALUES (13,'Francis','Ford','M');
 INSERT INTO filmworker VALUES (14,'Martin','Scorsese','M');
 INSERT INTO filmworker VALUES (15,'Stanley','Kubrick','M');
--- --------------Aqui quede
-INSERT INTO filmworker VALUES (16,'Keanu','Reeves','M');
-INSERT INTO filmworker VALUES (17,'Jordana','Brewster','F');
-INSERT INTO filmworker VALUES (18,'Daniel','Brühl','M');
-INSERT INTO filmworker VALUES (19,'Rachel','McAdams','F');
-INSERT INTO filmworker VALUES (20,'Jennifer','Aniston','F');
-INSERT INTO filmworker VALUES (21,'Scarlett','Johansson','F');
-INSERT INTO filmworker VALUES (22,'Chris','Evans','M');
-INSERT INTO filmworker VALUES (23,'Keanu','Reeves','M');
-INSERT INTO filmworker VALUES (24,'Jordana','Brewster','F');
-INSERT INTO filmworker VALUES (25,'Daniel','Brühl','M');
+
+INSERT INTO filmworker VALUES (16,'Alfred','Hitchcock','M');
+INSERT INTO filmworker VALUES (17,'Orson','Welles','M');
+INSERT INTO filmworker VALUES (18,'Steven','Spielberg','M');
+INSERT INTO filmworker VALUES (19,'Quentin','Tarantino','M');
+INSERT INTO filmworker VALUES (20,'Kathryn','Bigelow','F');
+INSERT INTO filmworker VALUES (21,'Alice','Guy','F');
+INSERT INTO filmworker VALUES (22,'Lois','Weber','F');
+INSERT INTO filmworker VALUES (23,'Elena','Jordi','F');
+INSERT INTO filmworker VALUES (24,'Lotte','Reiniger','F');
+INSERT INTO filmworker VALUES (25,'Helena','Cortesina','F');
 
 
-
-
+-- Director--
+INSERT INTO director VALUES (12);
+INSERT INTO director VALUES (13);
+INSERT INTO director VALUES (14);
+INSERT INTO director VALUES (15);
+INSERT INTO director VALUES (16);
+INSERT INTO director VALUES (17);
+INSERT INTO director VALUES (18);
+INSERT INTO director VALUES (19);
+INSERT INTO director VALUES (20);
+INSERT INTO director VALUES (21);
+INSERT INTO director VALUES (22);
+INSERT INTO director VALUES (23);
+INSERT INTO director VALUES (24);
+INSERT INTO director VALUES (25);
 
 -- Performer--
 INSERT INTO performer VALUES (1);
