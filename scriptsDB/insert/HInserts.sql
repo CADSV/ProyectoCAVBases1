5593c1129652838ccf59a34a7e98ab777cf216c5
LAPTOP-4J8K1UJL\Vicente <vicentemirabal16@gmail.com>
2021-07-24 09:06:28 -0400

    Se agregaron inserciones a filmworker

--- scriptsDB/insert/HInserts.sql
+++ scriptsDB/insert/HInserts.sql
@@ -12,6 +12,25 @@ INSERT INTO filmworker VALUES (8,'Chris','Evans','M');
 INSERT INTO filmworker VALUES (9,'Keanu','Reeves','M');
 INSERT INTO filmworker VALUES (10,'Jordana','Brewster','F');
 INSERT INTO filmworker VALUES (11,'Daniel','Brühl','M');
+-- Agregados 
+INSERT INTO filmworker VALUES (12,'Clint','Eastwood','M');
+INSERT INTO filmworker VALUES (13,'Francis','Ford','M');
+INSERT INTO filmworker VALUES (14,'Martin','Scorsese','M');
+INSERT INTO filmworker VALUES (15,'Stanley','Kubrick','M');
+-- --------------Aqui quede
+INSERT INTO filmworker VALUES (16,'Keanu','Reeves','M');
+INSERT INTO filmworker VALUES (17,'Jordana','Brewster','F');
+INSERT INTO filmworker VALUES (18,'Daniel','Brühl','M');
+INSERT INTO filmworker VALUES (19,'Rachel','McAdams','F');
+INSERT INTO filmworker VALUES (20,'Jennifer','Aniston','F');
+INSERT INTO filmworker VALUES (21,'Scarlett','Johansson','F');
+INSERT INTO filmworker VALUES (22,'Chris','Evans','M');
+INSERT INTO filmworker VALUES (23,'Keanu','Reeves','M');
+INSERT INTO filmworker VALUES (24,'Jordana','Brewster','F');
+INSERT INTO filmworker VALUES (25,'Daniel','Brühl','M');
+
+
+
 
 
 -- Performer--
