ec736f2d31ea3f0b425474119dc0b7c3b6b33226
Carlos Doffiny S-V <47164433+CADSV@users.noreply.github.com>
2021-07-31 19:58:08 -0400

    Detalles reporte G

--- scriptsDB/view/G_View.sql
+++ scriptsDB/view/G_View.sql
@@ -4,14 +4,15 @@
 -- TERCERA ENTREGA
 
 CREATE VIEW reporte_G AS
-SELECT TitleCont, ReleaseYearCont, IsOriginalCont, ReqSusCont, FeatureRunTime, Description, Rating
-FROM FeatureContent
+SELECT FeatureContent.IdContent, TitleCont, ReleaseYearCont, IsOriginalCont, ReqSusCont, FeatureRunTime, CAST(AVG(HasSeen.Rating) AS DECIMAL(10,2)) AS Rating
+FROM FeatureContent 
 INNER JOIN HasSeen ON FeatureContent.IdContent = HasSeen.IdContent
 INNER JOIN Stars ON FeatureContent.IdContent = Stars.IdContent 
 INNER JOIN FilmWorker ON Stars.IdWorker = FilmWorker.IdWorker
 INNER JOIN Performer ON FilmWorker.IdWorker = Performer.IdWorker
-WHERE WorkerName = "Daniel" AND WorkerLastName = "Brühl"
-HAVING Rating > (SELECT AVG(Rating) -- Se debe hacer subconsulta para obtener el promedio de todas las películas en general
+WHERE (WorkerName = 'Daniel') AND (WorkerLastName = 'Brühl')
+GROUP BY FeatureContent.IdContent
+HAVING AVG(HasSeen.Rating) > (SELECT AVG(Rating) -- Se debe hacer subconsulta para obtener el promedio de todas las películas en general
                  FROM HasSeen
                  INNER JOIN FeatureContent ON HasSeen.IdContent = FeatureContent.IdContent)
-ORDER BY Rating DESC
+ORDER BY AVG(HasSeen.Rating) DESC;
