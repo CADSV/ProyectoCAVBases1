afe1a90ae3202644b096a564db26cad621a79c03
Alejandro Pestana <alejandropestana@outlook.com>
2021-07-31 17:06:16 -0400

    Hecho el View del reporte G, arreglado conflicto extraño con el reporte I

--- scriptsDB/view/G_View.sql
+++ scriptsDB/view/G_View.sql
@@ -2,3 +2,16 @@
 -- mayor al promedio de películas y haya actuado “Daniel Brühl”.
 
 -- TERCERA ENTREGA
+
+CREATE VIEW reporte_G AS
+SELECT TitleCont, ReleaseYearCont, IsOriginalCont, ReqSusCont, FeatureRunTime, Description, Rating
+FROM FeatureContent
+INNER JOIN HasSeen ON FeatureContent.IdContent = HasSeen.IdContent
+INNER JOIN Stars ON FeatureContent.IdContent = Stars.IdContent 
+INNER JOIN FilmWorker ON Stars.IdWorker = FilmWorker.IdWorker
+INNER JOIN Performer ON FilmWorker.IdWorker = Performer.IdWorker
+WHERE WorkerName = "Daniel" AND WorkerLastName = "Brühl"
+HAVING Rating > (SELECT AVG(Rating) -- Se debe hacer subconsulta para obtener el promedio de todas las películas en general
+                 FROM HasSeen
+                 INNER JOIN FeatureContent ON HasSeen.IdContent = FeatureContent.IdContent)
+ORDER BY Rating DESC
