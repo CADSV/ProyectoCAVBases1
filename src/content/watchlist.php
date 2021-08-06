981a497cfca1f6a4489bbe7755c1e85a266207cb
Carlos Doffiny S-V <47164433+CADSV@users.noreply.github.com>
2021-08-06 12:17:54 -0400

    Terminado el watchlist. Ya se agregan y eliminan fino en su correspondiente interfaz

--- src/content/watchlist.php
+++ src/content/watchlist.php
@@ -1,11 +1,46 @@
-// Aquí va el watchlist (Mi lista). Se deben hacer o modificar funciones para obtener la lista de contenidos que tiene el perfil 
-// en la tabla watchlist.
+<?php
 
-// Necesitan una nueva función en contentProvider, o agregarle una categoría 3 y un condicional para editar el query, y 
-// obtener los contenidos de watchlist.
+require_once("../../data/config.php");
+require_once("../../data/providers/contentProvider.php");
+require_once("../../data/containers/categoryContainer.php");
+require_once("../../data/containers/watchlistContainer.php");
+include_once("navBar.php");
+require_once("content.php");
+require_once("header.php");
 
-// Si quieren mostrar la watchlist por categorías, simplemente usan la categoría 3 al llamar a categoryContainer, y dependiendo de lo anterior
-// hacen un nuevo condicional o nada respectivamente.
 
-// No es necesario un preview aquí, pero si quieren pueden hacerlo. Tendrían que hacer una o dos funciones más, o usar la categoría 3 como
-// coloqué anteriormente.
+if (!isset($_SESSION["userLoggedIn"])){
+    header("Location: ../../index.php");
+}
+
+if (!isset($_SESSION["IdProfile"])){
+    header("Location: ../profile/selectProfile.php");
+}
+
+$userLoggedIn = $_SESSION["userLoggedIn"];
+
+$IdProfile = $_SESSION["IdProfile"];
+
+$watchlist = new WatchlistContainer($connection, $userLoggedIn);
+echo $watchlist->showWatchlist($IdProfile);
+
+?>
+    <!DOCTYPE html>
+    <html lang='es'>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport"
+                content="width=device-width, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0, user-scalable=no">
+            <title>Carlevix</title>
+            <link rel="stylesheet" type="text/css" href="../../assets/style/contentStyle.css"/> 
+            <script src="https://kit.fontawesome.com/06a651c8da.js" crossorigin="anonymous"> </script>
+
+        </head>
+        <body>
+            <div class="watchlistWrapper">
+
+              
+            </div>
+        </div>   
+    </html>         
